e68b64bb6c8ac137d5f2b9ea1c330fcd994e2f59
jantpedraza <jantpedraza@gmail.com>
2019-10-28 20:26:39 +0100

    Search Controller scafolding

---
+++ src/main/java/com/banking/accountservice/web/SearchController.java
@@ -0,0 +1,14 @@
+package com.banking.accountservice.web;
+
+import com.banking.accountservice.dto.Account;
+
+import java.util.Collections;
+import java.util.List;
+
+// TODO: Implement search logic
+public class SearchController {
+
+    List<Account> search(String regex) {
+        return Collections.emptyList();
+    }
+}
